2f5f22d78e3478958c44d60d64b0f8d7259205d7
guischpor <guischpor1@live.com>
2019-01-19 14:58:06 -0200

    update

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -5,49 +5,177 @@ import {
     View,
     TextInput,
     TouchableOpacity,
-    TouchableHighlight
+    TouchableHighlight,
+    Dimensions
 } from 'react-native';
 
+import {Font} from 'expo';
+import {
+    MaterialIcons,
+    FontAwesome,
+    MaterialCommunityIcons
+} from '@expo/vector-icons';
+
+
+const {width: WIDTH} = Dimensions.get('window');
+
 export default class FormLogin extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fontLoaded: false,
+            login: '',
+            pass: '',
+            showPass: true,
+            press: false,
+        }
+    }
+
+    async componentDidMount() {
+        await Font.loadAsync({
+            'Azonix': require('../fonts/Azonix.otf'),
+            'Lato-Bold': require('../fonts/Lato-Bold.ttf'),
+            'Lato-Light': require('../fonts/Lato-Light.ttf'),
+            'Lato-Medium': require('../fonts/Lato-Medium.ttf'),
+            'Lato-Regular': require('../fonts/Lato-Regular.ttf'),
+        });
+        this.setState({
+            fontLoaded: true
+        });
+    }
+
+    showPass = () => {
+        if (this.state.press == false) {
+            this.setState({
+                showPass: false,
+                press: true
+            });
+        } else {
+            this.setState({
+                showPass: true,
+                press: false
+            });
+        }
+    }
+
+
     render() {
+        if (this.state.fontLoaded != true) {
+            return (
+                <View />
+            )
+        }
+
     return (
-        <View style={styles.container}>
-            <View>
-                <Text>PLAY CHAT</Text>
-                <Text>Let's Make Conversation</Text>
+        <View style={styles.viewContainer}>
+
+            <View style={styles.txtContainer}>
+                <View style={{flexDirection: 'row'}}>
+                    <Text style={styles.txtTitlePlay}>PLAY </Text>
+                    <Text style={styles.txtTitleChat}>CHAT</Text>
+                </View>
+                <Text style={styles.txtTitleLets}>Let's Make Conversation</Text>
             </View>
 
-            <View>
-                <TextInput
-                    placeholder='Username'
-                    style={{
-                        backgroundColor: ''
-                    }}
-                />
-                <TextInput
-                    placeholder='Password'
-                    secureTextEntry={true}
-                />
-                <Text>Have not registered yet?.</Text>
-
-                <TouchableOpacity>
-                    <Text>Register now.</Text>
+            <View style={styles.inputContainer}>
+                <View style={styles.inputStylesUser}>
+                    <MaterialIcons
+                        name="person"
+                        size={25}
+                        color={'#9DAAEA'}
+                        style={{
+                            backgroundColor: 'transparent',
+                            position: 'absolute',
+                            justifyContent: 'center',
+                            top: 14,
+                            left: 19
+                        }}
+                    />
+                    <TextInput
+                        placeholder='Username'
+                        placeholderTextColor='#9DAAEA'
+                        autoCapitalize='none'
+                        value={this.state.login}
+                        onChangeText={(login) => this.setState({login: login})}
+                        style={styles.txtInputUser}
+                    />
+                </View>
+                <View style={styles.inputStylesPass}>
+                    <FontAwesome
+                        name="lock"
+                        size={25}
+                        color={'#9DAAEA'}
+                        style={{
+                            backgroundColor: 'transparent',
+                            position: 'absolute',
+                            justifyContent: 'center',
+                            top: 14,
+                            left: 23
+                        }}
+                    />
+                    <TextInput
+                        placeholder='Password'
+                        secureTextEntry={this.state.showPass}
+                        placeholderTextColor='#9DAAEA'
+                        style={styles.txtInputPass}
+                        value={this.state.pass}
+                        onChangeText={(pass) => this.setState({pass: pass})}
+                        autoCapitalize='none'
+                    />
+                    <TouchableOpacity
+                            style={styles.eyeBtn}
+                            onPress={this.showPass.bind(this)}
+                        >
+                            {this.state.press === false ?
+                                <MaterialCommunityIcons
+                                    name="eye-off-outline"
+                                    size={25}
+                                    color={'rgb(150,150,150)'}
+                                    style={{backgroundColor: 'transparent'}}
+                                    color={'#9DAAEA'}
+                                />
+                            :
+                                <MaterialCommunityIcons
+                                    name="eye-outline"
+                                    size={25}
+                                    color={'#0b7dfa'}
+                                    style={{backgroundColor: 'transparent'}}
+                                />
+                            }
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity
+                    style={{top: 20}}
+                >
+                    <Text style={styles.txtForgotPass}>Esqueci minha senha</Text>
                 </TouchableOpacity>
             </View>
 
-            <View>
-            <TouchableHighlight
-                underlayColor={'#0b7dfa'}
-                activeOpacity={0.3}
-                onPress={() => false}
-                style={{
-                    borderRadius: 12,
-                }}
-            >
-                <Text style={styles.txtSingIn}>
-                    Sign In
-                </Text>
-            </TouchableHighlight>
+            <View style={styles.btnContainer}>
+                <TouchableHighlight
+                    underlayColor={'#0b7dfa'}
+                    activeOpacity={0.3}
+                    onPress={() => false}
+                    style={{borderRadius: 8,}}
+                >
+                    <Text style={styles.txtSingIn}>
+                        SIGN IN
+                    </Text>
+                </TouchableHighlight>
+
+                <TouchableOpacity
+                    style={{
+                        flexDirection: 'row',
+                        top: 30,
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Text style={styles.txtCriar}>Criar </Text>
+                    <Text style={styles.txtConta}>nova </Text>
+                    <Text style={styles.txtConta}>Conta</Text>
+                </TouchableOpacity>
+
             </View>
         </View>
         );
@@ -55,20 +183,101 @@ export default class FormLogin extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
+    viewContainer: {
         flex: 1,
-        backgroundColor: '#1f22cf',
+        backgroundColor: '#161C5C',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    txtContainer : {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        top: 20,
+    },
+    txtTitlePlay: {
+        fontFamily: 'Azonix',
+        color: '#fff',
+        fontSize: 48
+    },
+    txtTitleChat: {
+        fontFamily: 'Azonix',
+        color: '#0b7dfa',
+        fontSize: 48
+    },
+    txtTitleLets: {
+        fontFamily: 'Azonix',
+        color: '#fff',
+        fontSize: 19
+    },
+    inputContainer : {
+        flex: 2,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputStylesUser: {
+        top: 10,
+        backgroundColor: 'rgba(255,255,255,0.1)',
+        width: WIDTH -70,
+        marginBottom: 27,
+        padding: 14,
+        borderRadius: 8,
+        flexDirection: 'row',
+    },
+    inputStylesPass: {
+        top: 10,
+        backgroundColor: 'rgba(255,255,255,0.1)',
+        width: WIDTH -70,
+        marginBottom: 27,
+        padding: 14,
+        borderRadius: 8,
+        flexDirection: 'row',
+    },
+    txtInputUser:{
+        color: '#9DAAEA',
+        fontFamily: 'Lato-Regular',
+        fontSize: 15,
+        left: 48
+    },
+    txtInputPass:{
+        color: '#9DAAEA',
+        fontFamily: 'Lato-Regular',
+        fontSize: 15,
+        left: 48
+    },
+    txtForgotPass:{
+        color: '#9DAAEA',
+        fontFamily: 'Lato-Regular',
+        fontSize: 15
+    },
+    btnContainer : {
+        flex: 2,
+        top: 15
     },
     txtSingIn: {
         textAlign:'center',
         backgroundColor: '#0b7dfa',
-        borderRadius: 12,
+        borderRadius: 8,
         color: '#fff',
-        fontWeight: 'bold',
         fontSize: 18,
-        padding: 10,
-        width: 350,
+        padding: 14,
+        width: WIDTH -70,
+        fontFamily: 'Lato-Regular',
+    },
+    txtCriar: {
+        color: '#fff',
+        fontFamily: 'Lato-Regular',
+        fontSize: 15
+    },
+    txtConta: {
+        color: '#3D91EE',
+        fontFamily: 'Lato-Bold',
+        fontSize: 15
+    },
+    eyeBtn: {
+        top: 14,
+        right: 20,
+        alignItems:'flex-end',
+        position: 'absolute'
     },
 });
