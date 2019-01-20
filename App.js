c2e8a78995215007a83200e6b5bbfb4d9d1ee4d7
guischpor <guischpor1@live.com>
2019-01-20 12:41:26 -0200

    .

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 //React and React native import
 import React from 'react';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
 //Components  and Elements local
 import Route from './src/components/Route';
+import reducers from './src/reducers/Index';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Route />
+      <Provider store={createStore(reducers)}>
+        <Route />
+      </Provider>
     );
   }
 }
